5c90d3e3ea52988baf0212838deb1cbe3c8f42bd
Alfaben <zrav420@gmail.com>
2022-05-27 12:26:11 +0700

    update queue page

--- pages/queue/index.tsx
+++ pages/queue/index.tsx
@@ -6,6 +6,7 @@ import { AxiosResponse } from "axios";
 import { useEffect, useState } from "react";
 import api from "../../utils/api";
 import moment from "moment";
+import Link from "next/link";
 
 const QueuePage = () => {
     const [isLoading, setIsLoading] = useState(false);
@@ -31,36 +32,72 @@ const QueuePage = () => {
     return (
         <>
             <NavHeader />
-            <Grid
-                data={result}
-                columns={[
-                    {
-                        id: "event",
-                        name: "Event",
-                    },
-                    {
-                        id: "ticket",
-                        name: "Ticket",
-                    },
-                    {
-                        id: "status",
-                        name: "Status",
-                    },
-                    {
-                        id: "time",
-                        name: "time",
-                        formatter: (cell: number) =>
-                            moment.unix(+cell).format("LLL"),
-                    },
-                ]}
-                search={true}
-                sort={true}
-                autoWidth={true}
-                pagination={{
-                    enabled: true,
-                    limit: 20,
-                }}
-            />
+            {result.length == 0 ? (
+                <>
+                    <div className="mx-auto lg:py-16 lg:flex lg:items-center lg:justify-between">
+                        <h2 className="font-extrabold tracking-tight text-gray-900">
+                            <span className="block">
+                                No queue at this time!
+                            </span>
+                            <br />
+                            <span className="text-3xl sm:text-4xl block">
+                                Ready to dive in?
+                            </span>
+                            <span className="text-2xl sm:text-4xl block bg-pir-green">
+                                Start claim your NFT today.
+                            </span>
+                        </h2>
+                        <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+                            <div className="inline-flex rounded-md shadow">
+                                <Link href="/claim">
+                                    <a className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md bg-pir-green text-black">
+                                        Get started
+                                    </a>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <Grid
+                    data={result}
+                    columns={[
+                        {
+                            id: "event",
+                            name: "Event",
+                        },
+                        {
+                            id: "ticket",
+                            name: "Ticket",
+                        },
+                        {
+                            id: "status",
+                            name: "Status",
+                        },
+                        {
+                            id: "time",
+                            name: "Time",
+                            formatter: (cell: string) =>
+                                moment.unix(+cell).format("LLL"),
+                        },
+                        {
+                            id: "body",
+                            name: "Address",
+                            data: (row: any) =>
+                                row.body.toAddress
+                                    ? row.body.toAddress
+                                    : "Self Contract",
+                        },
+                    ]}
+                    search={true}
+                    sort={true}
+                    autoWidth={true}
+                    pagination={{
+                        enabled: true,
+                        limit: 20,
+                    }}
+                />
+            )}
             <NavFooter />
         </>
     );
